b356aa8798d60fae2ef11fa6d75aa884bd045bcf
Phuc Nguyen <phucnd1303@gmail>
2026-03-13 07:52:20 +0700

    feat: Implement hash map solution for Two Sum problem

---
+++ array&hashing/two-sum/hash-map.js
@@ -0,0 +1,29 @@
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @param {number} target
+   * @return {number[]}
+   */
+  twoSum(nums, target) {
+    const indices = {};
+
+    for (let i = 0; i <= nums.length; i++) {
+      indices[nums[i]] = i;
+    }
+
+    for (let i = 0; i <= nums.length; i++) {
+      let diff = target - nums[i];
+
+      if (indices[diff] !== undefined && indices[diff] !== i) {
+        return [i, indices[diff]];
+      }
+    }
+
+    return [];
+  }
+}
+
+const solution = new Solution();
+const result = solution.twoSum([2, 7, 11, 15], 17);
+
+console.log(result);
